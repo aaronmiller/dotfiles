0afb2117ef5640d3c468482025371fb662680b83
Aaron Miller <aaron.miller@braze.com>
2024-04-12 11:27:57 -0400

    adding more devdocs custom keybindings

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -147,9 +147,12 @@
 
 (use-package! devdocs
   :config
-  (map! :ne "SPC d i" #'devdocs-install
+  (map! :ne "SPC d d" #'devdocs-delete
+        :ne "SPC d i" #'devdocs-install
         :ne "SPC d l" #'devdocs-lookup
-        :ne "SPC d p" #'devdocs-peruse))
+        :ne "SPC d p" #'devdocs-peruse
+        :ne "SPC d s" #'devdocs-search
+        :ne "SPC d u" #'devdocs-update-all))
 
 (use-package! doom
   :config
