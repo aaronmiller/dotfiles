01d06fb60d981911956d687a6a3ffa77d630ce06
Aaron Miller <amiller719@gmail.com>
2024-04-14 02:22:40 -0400

    removing google-make-newline-indent func for c-mode-common-hook

--- .doom.d/hooks.el
+++ .doom.d/hooks.el
@@ -1,7 +1,6 @@
 ;;; $DOOMDIR/hooks.el -*- lexical-binding: t; -*-
 
 (add-hook! 'c-mode-common-hook #'google-set-c-style)
-(add-hook! 'c-mode-common-hook #'google-make-newline-indent)
 
 (add-hook 'go-mode-hook
           (lambda ()
