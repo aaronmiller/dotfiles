7f020446b09e798d355ac1fc1ff9b12357017177
Aaron Miller <amiller719@gmail.com>
2024-05-26 13:17:23 -0400

    fixing config

--- .doom.d/hooks.el
+++ .doom.d/hooks.el
@@ -2,11 +2,6 @@
 
 (add-hook! 'c-mode-common-hook #'google-set-c-style)
 
-(with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs
-               `((c++-mode c-mode) . ,(eglot-alternatives
-                                       '(("clangd-8"))))))
-
 (add-hook 'python-mode-hook
           (lambda ()
             (add-hook 'before-save-hook #'py-isort-buffer nil t)))
