acbde06457087cdd98a53c2fc1c132cddb632172
Aaron Miller <aaron.miller@braze.com>
2024-07-18 14:31:38 -0400

    switching to source code pro font

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -15,9 +15,9 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq! doom-font (font-spec :family "Hack Nerd Font Mono" :size 12)
-       doom-symbol-font (font-spec :family "Symbols Nerd Font Mono")
-       doom-variable-pitch-font (font-spec :family "Hack Nerd Font Mono"))
+(setq! doom-font (font-spec :family "Source Code Pro" :size 12)
+       doom-symbol-font (font-spec :family "Source Code Pro")
+       doom-variable-pitch-font (font-spec :family "Source Code Pro"))
 
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
