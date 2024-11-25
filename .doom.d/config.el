86247840063cbd949102b4035c620bb557323291
Aaron Miller <amiller719@gmail.com>
2024-11-25 17:35:37 -0500

    updating font size and color for moonlight-wilmersdorf theme

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Hack Nerd Font Mono" :size 12)
-      doom-variable-pitch-font (font-spec :family "Hack Nerd Font Mono" :size 12))
+(setq doom-font (font-spec :family "Hack Nerd Font Mono" :size 14)
+      doom-variable-pitch-font (font-spec :family "Hack Nerd Font Mono" :size 14))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
