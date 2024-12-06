c23acf3f37fbce892cc2f98cf3c920e36f610b79
Aaron Miller <amiller719@gmail.com>
2024-12-06 18:19:38 -0500

    sorting hooks

--- .doom.d/hooks.el
+++ .doom.d/hooks.el
@@ -1,23 +1,21 @@
 ;;; $DOOMDIR/hooks.el -*- lexical-binding: t; -*-
 
-(add-hook! 'c-mode-common-hook #'google-set-c-style)
-
 (add-hook 'python-mode-hook
           (lambda ()
             (add-hook 'before-save-hook #'py-isort-buffer nil t)))
 
-(add-hook! 'jinja2-mode-hook #'toggle-truncate-lines)
-(add-hook! 'projectile-after-switch-project-hook #'treemacs-find-file)
-(add-hook! 'sh-mode-hook #'shfmt-on-save-mode)
-
 (add-hook! '(conf-mode-hook prog-mode-hook))
-
-(add-hook! 'vterm-mode-hook #'evil-emacs-state)
-(add-hook! 'yaml-mode-hook #'toggle-truncate-lines)
 
 (add-hook! '+doom-dashboard-functions
   (hide-mode-line-mode)
   (hl-line-mode -1))
+
+(add-hook! 'c-mode-common-hook #'google-set-c-style)
+(add-hook! 'jinja2-mode-hook #'toggle-truncate-lines)
+(add-hook! 'projectile-after-switch-project-hook #'treemacs-find-file)
+(add-hook! 'sh-mode-hook #'shfmt-on-save-mode)
+(add-hook! 'vterm-mode-hook #'evil-emacs-state)
+(add-hook! 'yaml-mode-hook #'toggle-truncate-lines)
 
 (remove-hook! '+doom-dashboard-functions
   #'doom-dashboard-widget-banner
