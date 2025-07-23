20e23e322446c4bb9c0f0fa9369c5f897dc6fdda
Aaron Miller <amiller719@gmail.com>
2025-07-23 17:23:20 -0400

    disabling before save hooks

--- .doom.d/hooks.el
+++ .doom.d/hooks.el
@@ -1,8 +1,8 @@
 ;;; $DOOMDIR/hooks.el -*- lexical-binding: t; -*-
 
-(add-hook 'python-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook #'py-isort-buffer nil t)))
+;; (add-hook 'python-mode-hook
+;;           (lambda ()
+;;             (add-hook 'before-save-hook #'py-isort-buffer nil t)))
 
 (add-hook! '(conf-mode-hook prog-mode-hook))
 
@@ -13,7 +13,7 @@
 (add-hook! 'c-mode-common-hook #'google-set-c-style)
 (add-hook! 'jinja2-mode-hook #'toggle-truncate-lines)
 (add-hook! 'projectile-after-switch-project-hook #'treemacs-find-file)
-(add-hook! 'sh-mode-hook #'shfmt-on-save-mode)
+;; (add-hook! 'sh-mode-hook #'shfmt-on-save-mode)
 (add-hook! 'vterm-mode-hook #'evil-emacs-state)
 (add-hook! 'yaml-mode-hook #'toggle-truncate-lines)
 
