f81e0e03c9a8e1137b5937943d5d850714121f80
Aaron Miller <amiller719@gmail.com>
2026-04-28 14:39:01 -0400

    updating nix config

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,22 +1,31 @@
 { pkgs, lib, config, ... }:
 
 {
-  programs.git = {
-    enable = true;
-    extraConfig = {
-      core.editor = "vim";
-      pull.rebase = true;
-      fetch.prune = true;
-      init.defaultBranch = "main";
-      push.autoSetupRemote = true;
-      diff.colorMoved = "zebra";
-      advice.addIgnoredFile = false;
-      remote.origin.prune = true;
-      include.path = "~/.gitconfig_user";
-    };
-    ignores = [
-      ".DS_Store"
-      "*.swp"
-    ];
-  };
+  environment.etc."gitconfig".text = ''
+    [core]
+      editor = vim
+    [pull]
+      rebase = true
+    [fetch]
+      prune = true
+    [init]
+      defaultBranch = main
+    [push]
+      autoSetupRemote = true
+    [diff]
+      colorMoved = zebra
+    [advice]
+      addIgnoredFile = false
+    [remote "origin"]
+      prune = true
+    [include]
+      path = ~/.gitconfig_user
+    [core]
+      excludesfile = /etc/gitignore
+  '';
+
+  environment.etc."gitignore".text = ''
+    .DS_Store
+    *.swp
+  '';
 }
