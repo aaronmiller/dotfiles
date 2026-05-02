919c57a42b3f06ead6962dd62fa4eb27f33822fc
Aaron Miller <amiller719@gmail.com>
2026-05-01 22:47:55 -0400

    cleaning up dotfiles

--- teardown.sh
+++ teardown.sh
@@ -1,32 +1,8 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -uo pipefail
 
 # If system-wide shell initialisation files haven't been altered since installing Nix, use the backups made by the installer
-if [ -f "/etc/zshrc.backup-before-nix" ]; then
-  sudo mv /etc/zshrc.backup-before-nix /etc/zshrc
-fi
-
-if [ -f "/etc/zshrc.before-nix-darwin" ]; then
-  sudo mv /etc/zshrc.before-nix-darwin /etc/zshrc
-fi
-
-if [ -f "/etc/bashrc.backup-before-nix" ]; then
-  sudo mv /etc/bashrc.backup-before-nix /etc/bashrc
-fi
-
-if [ -f "/etc/bashrc.before-nix-darwin" ]; then
-  sudo mv /etc/bashrc.before-nix-darwin /etc/bashrc
-fi
-
-if [ -f "/etc/bash.bashrc.backup-before-nix" ]; then
-  sudo mv /etc/bash.bashrc.backup-before-nix /etc/bash.bashrc
-fi
-
-if [ -f "/etc/bash.bashrc.before-nix-darwin" ]; then
-  sudo mv /etc/bash.bashrc.before-nix-darwin /etc/bash.bashrc
-fi
-
 # Otherwise, edit /etc/zshrc, /etc/bashrc, and /etc/bash.bashrc to remove the lines sourcing nix-daemon.sh, which should look like this:
 ## Nix
 #if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
@@ -34,44 +10,80 @@ fi
 #fi
 ## End Nix
 
-# Stop and remove the Nix daemon services
-sudo launchctl unload /Library/LaunchDaemons/org.nixos.nix-daemon.plist
-
-if [ -f "/Library/LaunchDaemons/org.nixos.nix-daemon.plist" ]; then
-  sudo rm /Library/LaunchDaemons/org.nixos.nix-daemon.plist
+if [ -f "/etc/zshrc.backup-before-nix" ]; then
+ /usr/bin/sudo /bin/mv /etc/zshrc.backup-before-nix /etc/zshrc
+else
+  EDITOR=/usr/bin/vi /etc/zshrc
 fi
 
-sudo launchctl unload /Library/LaunchDaemons/org.nixos.darwin-store.plist
+if [ -f "/etc/zshrc.before-nix-darwin" ]; then
+  /usr/bin/sudo /bin/mv /etc/zshrc.before-nix-darwin /etc/zshrc
+else
+  EDITOR=/usr/bin/vi /etc/zshrc
+fi
+
+if [ -f "/etc/bashrc.backup-before-nix" ]; then
+  /usr/bin/sudo /bin/mv /etc/bashrc.backup-before-nix /etc/bashrc
+else
+  EDITOR=/usr/bin/vi /etc/bashrc
+fi
+
+if [ -f "/etc/bashrc.before-nix-darwin" ]; then
+  /usr/bin/sudo /bin/mv /etc/bashrc.before-nix-darwin /etc/bashrc
+else
+  EDITOR=/usr/bin/vi /etc/bashrc
+fi
+
+if [ -f "/etc/bash.bashrc.backup-before-nix" ]; then
+  /usr/bin/sudo /bin/mv /etc/bash.bashrc.backup-before-nix /etc/bash.bashrc
+else
+  EDITOR=/usr/bin/vi /etc/bash.bashrc
+fi
+
+if [ -f "/etc/bash.bashrc.before-nix-darwin" ]; then
+  /usr/bin/sudo /bin/mv /etc/bash.bashrc.before-nix-darwin /etc/bash.bashrc
+else
+  EDITOR=/usr/bin/vi /etc/bash.bashrc
+fi
+
+# Stop and remove the Nix daemon services
+/usr/bin/sudo /bin/launchctl unload /Library/LaunchDaemons/org.nixos.nix-daemon.plist 2>/dev/null
+
+if [ -f "/Library/LaunchDaemons/org.nixos.nix-daemon.plist" ]; then
+  /usr/bin/sudo /bin/rm /Library/LaunchDaemons/org.nixos.nix-daemon.plist
+fi
+
+/usr/bin/sudo /bin/launchctl unload /Library/LaunchDaemons/org.nixos.darwin-store.plist 2>/dev/null
 
 if [ -f "/Library/LaunchDaemons/org.nixos.darwin-store.plist" ]; then
-  sudo rm /Library/LaunchDaemons/org.nixos.darwin-store.plist
+  /usr/bin/sudo /bin/rm /Library/LaunchDaemons/org.nixos.darwin-store.plist
 fi
 
 # Remove the nixbld group and the _nixbuildN users
-if [ sudo /usr/bin/dscl . -list /Groups | grep nixbld ]; then
-  sudo dscl . -delete /Groups/nixbld
-  for u in $(sudo dscl . -list /Users | grep _nixbld); do sudo dscl . -delete /Users/$u; done
+if /usr/bin/sudo /usr/bin/dscl . -list /Groups | /usr/bin/grep nixbld; then
+  /usr/bin/sudo /usr/bin/dscl . -delete /Groups/nixbld
+  for u in $(/usr/bin/sudo /usr/bin/dscl . -list /Users | /usr/bin/grep _nixbld); do /usr/bin/sudo /usr/bin/dscl . -delete /Users/$u; done
 fi
 
-# Edit fstab using sudo vifs to remove the line mounting the Nix Store volume on /nix, which looks like
+# Edit fstab using /usr/bin/sudo vifs to remove the line mounting the Nix Store volume on /nix, which looks like
 # UUID=<uuid> /nix apfs rw,noauto,nobrowse,suid,owners
 # or
 # LABEL=Nix\040Store /nix apfs rw,nobrowse
-EDITOR=vim sudo vifs
+EDITOR=/usr/bin/vi /usr/bin/sudo /usr/sbin/vifs
 
 # Edit /etc/synthetic.conf to remove the nix line. If this is the only line in the file you can remove it entirely
 if [ -f /etc/synthetic.conf ]; then
-  if [ "$(cat /etc/synthetic.conf)" = "nix" ]; then
-    sudo rm /etc/synthetic.conf
+  if [ "$(/bin/cat /etc/synthetic.conf)" = "nix" ]; then
+    /usr/bin/sudo /bin/rm /etc/synthetic.conf
   else
-    sudo vi /etc/synthetic.conf
+    /usr/bin/sudo /usr/bin/vi /etc/synthetic.conf
   fi
 fi
 
 # Remove the files Nix added to your system, except for the store
-sudo rm -rf /etc/nix /var/root/.nix-profile /var/root/.nix-defexpr /var/root/.nix-channels ~/.nix-profile ~/.nix-defexpr ~/.nix-channels ~/.local/share/nix ~/.local/state/nix ~/.cache/nix
+/usr/bin/sudo /bin/rm -rf /etc/nix /var/root/.nix-profile /var/root/.nix-defexpr /var/root/.nix-channels ~/.nix-profile ~/.nix-defexpr ~/.nix-channels ~/.local/share/nix ~/.local/state/nix ~/.cache/nix
 
 # Remove the Nix Store volume
-sudo /usr/sbin/diskutil apfs deleteVolume /nix
+/usr/bin/sudo /usr/sbin/diskutil apfs deleteVolume /nix 2>/dev/null
 
-#diskutil list
+/usr/sbin/diskutil list
