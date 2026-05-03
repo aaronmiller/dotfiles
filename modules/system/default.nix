22d17dbf696d1f71b2f3e0939df689402b97441e
Aaron Miller <amiller719@gmail.com>
2026-05-03 15:00:08 -0400

    progress

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -6,4 +6,9 @@
   system.configurationRevision = self.rev or self.dirtyRev or null;
   system.stateVersion = 6;
   nixpkgs.hostPlatform = "x86_64-darwin";
+
+  imports = [
+    ./env.nix
+    ./packages.nix
+  ];
 }
